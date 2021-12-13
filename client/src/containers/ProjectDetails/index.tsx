696214157506e830c6ed94fe7fa60b6a41e55ce3
sunnyno <eugenia.dzytsiuk@ukr.net>
2021-12-13 14:12:25 +0200

    build fix

--- client/src/containers/ProjectDetails/index.tsx
+++ client/src/containers/ProjectDetails/index.tsx
@@ -12,7 +12,7 @@ const ProjectDetails = React.memo( ({ projectData }: Props) => {
     return <Link to={ `catalogue/${projectData.id}` } className={ classes['project-details'] }>
         <div className={ classes['project-details__content'] }>
             <div className={ classes['project-details__img'] }>
-                <img src={ projectData.mainImage } alt={ projectData.id }/>
+                <img src={ projectData.mainImage } alt={ projectData.id + ""}/>
             </div>
             <div className={ classes['project-details__info'] }>
                  { /*<span>{ projectData.title }</span>*/ }
