dfc535db1603d09cb2e8feae21c84320a6054161
sunnyno <eugenia.dzytsiuk@ukr.net>
2022-01-16 21:03:27 +0200

    projects details options added

--- client/src/pages/Admin/constants.ts
+++ client/src/pages/Admin/constants.ts
@@ -2,21 +2,17 @@ const styleOptions = ['классический', 'современный'];
 
 const foundationOptions = [
   'ленточный',
-  'столбчатый',
-  'монолитный ленточный',
-  'свайный',
-  'мелкозаглубленный',
   'плитный',
-  'плавающий',
-  'винтовой',
+  'свайный',
+  'комбинированный (свайно-плитный)',
 ];
 
-const ceilingOptions = ['монолитная ж/б плита', 'сборное ж/б из плит', 'дерево'];
+const ceilingOptions = ['монолитная ж/б плита', 'сборное ж/б из плит', 'дерево', 'комбинированная'];
 
-const roofOptions = ['битумная черепица', 'металлочерепица', 'шифер'];
+const roofOptions = ['битумная черепица', 'металлочерепица', 'профнастил', 'фальцевая', 'плоская', 'черепица', 'сланцевая'];
 
 const wallMaterialOptions = ['кирпич', 'газоблок', 'пеноблок', 'керамоблок'];
 
-const insulationOptions = ['пенопласт', 'стекловата', 'эковата'];
+const insulationOptions = [ 'минеральная вата', 'пенополистерол', 'фибролит'];
 
 export { styleOptions, foundationOptions, ceilingOptions, roofOptions, wallMaterialOptions, insulationOptions };
