d1cc5639666b208cfa396d3e83b855a448e7a98c
GuchiTheHasky <guchithehasky@gmail.com>
2024-07-23 02:38:49 +0300

    updated WebConfiguration

--- appro-back/src/main/java/com/appro/configuration/web/WebConfiguration.java
+++ appro-back/src/main/java/com/appro/configuration/web/WebConfiguration.java
@@ -1,13 +1,43 @@
 package com.appro.configuration.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.view.BeanNameViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
 
 @Configuration
 @EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("indexView");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/assets/", "file:target/classes/static/assets/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/", "file:target/classes/static/");
+    }
+
+    @Bean
+    public BeanNameViewResolver beanNameViewResolver() {
+        return new BeanNameViewResolver();
+    }
+
+    @Bean
+    public InternalResourceView indexView() {
+        InternalResourceView view = new InternalResourceView();
+        view.setUrl("/index.html");
+        return view;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
