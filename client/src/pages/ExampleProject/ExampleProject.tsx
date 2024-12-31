f80da42d476118e7f9c1f136ea10f3e532fc3f6a
Vladyslava Podzirei <vladapodzirey@gmail.com>
2024-12-31 16:31:04 +0200

    Add content

--- client/src/pages/ExampleProject/ExampleProject.tsx
+++ client/src/pages/ExampleProject/ExampleProject.tsx
@@ -1,28 +1,26 @@
 import { Container } from "@/containers/hoc/Container/Container";
-import { Breadcrumbs } from '@/components/UI/Breadcrumbs/Breadcrumbs'
 import React from "react";
 
 export const ExampleProject = () => {
     return (
-        <div>
-            <Container>
-                <div><Breadcrumbs /></div>
-                <div>
-                    <h1>Пример проекта</h1>
-                </div>
-                <div>
-                    <h2>Архитектура и констуркции</h2>
-                </div>
-                <div>
-                    <h2>Вода и канализация</h2>
-                </div>
-                <div>
-                    <h2>Отопление и вентиляция</h2>
-                </div>
-                <div>
-                    <h2>Электрика</h2>
-                </div>
-            </Container>
-        </div>
-    )
+			<div>
+				<Container>
+					<div>
+						<h1>Пример проекта</h1>
+					</div>
+					<div>
+						<h2>Архитектура и констуркции</h2>
+					</div>
+					<div>
+						<h2>Вода и канализация</h2>
+					</div>
+					<div>
+						<h2>Отопление и вентиляция</h2>
+					</div>
+					<div>
+						<h2>Электрика</h2>
+					</div>
+				</Container>
+			</div>
+		)
 }
